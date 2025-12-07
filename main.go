c8e8d8f4a52971001014db198317e0686c8fd1e0
yelhaoua <yacobelhaouarii@gmail.com>
2025-12-07 18:13:04 +0100

    Adding the files and the style in the server

--- main.go
+++ main.go
@@ -3,27 +3,41 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 )
 
-func HandleForm(w http.ResponseWriter, r *http.Request) {
-	tem, err := template.ParseFiles("./files/form.html")
+var temp, errtemp = template.ParseGlob("files/*.html")
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if errtemp != nil {
+		fmt.Println(w, errtemp)
+	}
+
+	err := temp.ExecuteTemplate(w, "index.html", nil)
 	if err != nil {
 		fmt.Fprintln(w, err)
 	}
-	tem.Execute(w, nil)
-	fmt.Fprintln(w, r.URL.Path)
+
+}
+func handelForm(w http.ResponseWriter, r *http.Request) {
+	err := temp.ExecuteTemplate(w, "form.html", nil)
+	if err != nil {
+		fmt.Fprintln(w, err)
+	}
+}
+func handleAscii(w http.ResponseWriter, r *http.Request) {
+	err := temp.ExecuteTemplate(w, "ascii.html", "Yaakoub")
+	if err != nil {
+		fmt.Fprintln(w, err)
+	}
+
 }
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./files")))
-	http.HandleFunc("/form.html", HandleForm)
+	http.Handle("/files/", http.StripPrefix("/files/", http.FileServer(http.Dir("files"))))
 
-	err := http.ListenAndServe("localhost:8080", nil)
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		fmt.Println("the server run in thr port localhost:8080")
-	}
+	http.HandleFunc("/", home)
+	http.HandleFunc("/form.html", handelForm)
+	http.HandleFunc("/ascii.html", handleAscii)
+	http.ListenAndServe(":8080", nil)
 }
