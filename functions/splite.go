816bf8a879e2155bc9c0e3536cfabd74fc8c38fd
haitbenal <hichameaitbenalla@gamil.com>
2025-12-07 18:15:54 +0100

    ..

---
+++ functions/splite.go
@@ -0,0 +1,50 @@
+package AsciiWeb
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func Spite(input, style string) string {
+	out := ""
+	data, err := os.ReadFile("../Arts/"+style+".txt")
+	if err != nil {
+		return "error type 01"
+	}
+	file := string(data)
+	var final [][]string
+	Start := strings.Split(file, "\n")
+	str := strings.ReplaceAll(input, "\\n", "\n")
+	if strings.Trim(str, "\n") == "" {
+		for i := 0; i < len(input); i++ {
+			if input[i] == '\\' {
+				fmt.Println()
+			}
+		}
+		return  ""
+	}
+	splited := strings.Split(str, "\n")
+
+	for _, va := range splited {
+		for _, v := range va {
+			index := int(((v - 32) * 9) + 1)
+			final = append(final, Start[index:index+8])
+		}
+		if len(va) != 0 {
+			
+			for i := 0; i < 8; i++ {
+				for a := range final {
+					out += final[a][i]
+				}
+				if i != 7 {
+					out += "\n"
+				}
+			}
+			return  out
+		} else {
+			return ""
+		}
+	}
+	return  out
+}
--- functions/web.go
+++ functions/web.go
@@ -1,6 +1,39 @@
 package AsciiWeb
 
+import (
+	"log"
+	"net/http"
+)
 
-func Start()  {
-	
+func Start() {
+	fs := http.FileServer(http.Dir("../web"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "../web/index.html")
+	})
+	http.HandleFunc("/submit", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "Bad Request", http.StatusBadRequest)
+			return
+		}
+
+		userinput := r.FormValue("userinput")
+		style := r.FormValue("style")
+		po := 	Spite(userinput, style)
+		w.Write([]byte(po))
+		// log.Println("Style:", style)
+
+		// Call the local Spite function in this package instead of importing the package itself
+		
+	})
+
+	log.Println("✅ Server running at http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
