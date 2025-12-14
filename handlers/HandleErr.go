fbcc1aa1a55107468b3979bf255fde688d03fab7
hchaitbenal <hichameaitbenalla@gmail.com>
2025-12-14 11:03:30 +0100

    Fix

--- handlers/HandleErr.go
+++ handlers/HandleErr.go
@@ -16,9 +16,5 @@ func HandleErr(w http.ResponseWriter, descriptioin string, statusCode int) {
 	// render the err pop up
 	info := data{Code: statusCode, Description: descriptioin}
 	w.WriteHeader(statusCode)
-<<<<<<< HEAD
 	pages.ExecuteTemplate(w, "error.html", info)
-=======
-	pages.ExecuteTemplate(w, "Err.html", info)
->>>>>>> 4d29ef36ee2c91e8da7a0cf6b76c6d8e29396f7c
 }
