0d94fbfa00a6e1791276cec1454b4bccd28c7868
yelhaoua <yacobelhaouarii@gmail.com>
2025-12-15 18:38:12 +0100

    Add an error if the banner file doesn't exist

--- handlers/HandleAscii.go
+++ handlers/HandleAscii.go
@@ -12,29 +12,32 @@ func HandleAscii(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			HandleErr(w, "Internal Server Error", http.StatusInternalServerError)
 			return
-		} else {
-			form := r.Form
-			// cheking if the user manupulate in the form names
-			if !form.Has("name") || !form.Has("radio") {
-				HandleErr(w, "Bad Request", http.StatusBadRequest)
-				return
-			} else {
-				// extract the data from the form and render the html file and err if existe
-				name := r.FormValue("name")
-				fName := r.FormValue("radio")
-				if fName != "shadow.txt" && fName != "standard.txt" && fName != "thinkertoy.txt" {
-					HandleErr(w, "Bad Request", http.StatusBadRequest)
-					return
-				}
-				splited := asciiart.Splite(fName)
-				res := asciiart.PrintSymbole(splited, name)
-				asciiErr := temp.ExecuteTemplate(w, "ascii-art.html", res)
-				if asciiErr != nil {
-					HandleErr(w, "Internal Server Error", http.StatusInternalServerError)
-					return
-				}
-			}
 		}
+		form := r.Form
+		// cheking if the user manupulate in the form names
+		if !form.Has("name") || !form.Has("radio") {
+			HandleErr(w, "Bad Request", http.StatusBadRequest)
+			return
+		}
+		// extract the data from the form and render the html file and err if existe
+		name := r.FormValue("name")
+		fName := r.FormValue("radio")
+		if fName != "shadow.txt" && fName != "standard.txt" && fName != "thinkertoy.txt" {
+			HandleErr(w, "Bad Request", http.StatusBadRequest)
+			return
+		}
+		splited := asciiart.Splite(fName)
+		if len(splited) == 0 {
+			HandleErr(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		res := asciiart.PrintSymbole(splited, name)
+		asciiErr := temp.ExecuteTemplate(w, "ascii-art.html", res)
+		if asciiErr != nil {
+			HandleErr(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
 	} else {
 		HandleErr(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
