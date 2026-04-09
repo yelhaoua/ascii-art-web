41066879acafb559868d2ff1c44e0b94425764d0
yelhaoua <yacobelhaouarii@gmail.com>
2026-04-09 16:54:43 +0100

    Finle ver

--- handlers/HandleDownlaod.go
+++ handlers/HandleDownlaod.go
@@ -3,19 +3,23 @@ package handlers
 import (
 	"net/http"
 	"os"
+	"strconv"
 )
 
 func HandleDownlaod(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 
 		filePath := "./files/AsciiArte.txt"
-		if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		file, err := os.Stat(filePath)
+		if err != nil {
 			HandleErr(w, "InternalServerError", http.StatusInternalServerError)
 			return
 		}
 
+		fileSize := strconv.FormatInt(file.Size(), 10)
 		w.Header().Set("Content-Disposition", "attachment; filename=AsciiArtDown.txt")
 		w.Header().Set("Content-Type", "application/octet-stream")
+		w.Header().Set("Content-Length", fileSize)
 
 		http.ServeFile(w, r, filePath)
 
